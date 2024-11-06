167f2aeb287ee5bf4f290617ab174518594eb529
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-11-05 18:27:31 -0800

    passing function as value to prop

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,6 +5,10 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  function handleSelect() {
+    console.log(`Selected!`);
+  }
+
   return (
     <div>
       <Header />
@@ -21,10 +25,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
         <h2>Time to get started!</h2>
--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,11 +1,7 @@
-export default function TabButton(props) {
-    function handleClick() {
-        console.log(`Clicked on ${props.children}`);
-    }
-
+export default function TabButton({children, onSelect}) {
     return (
         <li>
-            <button onClick={handleClick}>{props.children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     );
 }
