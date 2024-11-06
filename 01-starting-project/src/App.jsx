86d8a485480a271bb836f80b94a71352ea4294de
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-11-05 17:21:37 -0800

    props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png'
+import compImg from './assets/components.png'
+import { CORE_CONCEPTS } from './data';
 
 const dynamicContent = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,11 +23,29 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="core UI" img={compImg}/>
+            <CoreConcept title="Props" description="core UI" img={compImg}/>
+            <CoreConcept title="Components" description="core UI" img={compImg}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
