9fbde1b17daf14697b7d12d41b2f4c917f07588c
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-11-05 17:59:57 -0800

    components in files

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,7 @@
-import reactImg from './assets/react-core-concepts.png'
-import compImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const dynamicContent = ['Fundamental', 'Crucial', 'Core'];
-
-function getRandomNumber(max) {
-  return Math.floor(Math.random() * (max+1));
-}
-
-function Header() {
-  const word = dynamicContent[getRandomNumber(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {word} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -41,9 +11,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} img={CORE_CONCEPTS[0].image}/>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
             <CoreConcept {...CORE_CONCEPTS[1]}/>
             <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
         <h2>Time to get started!</h2>
---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImg from '../assets/react-core-concepts.png'
+
+const dynamicContent = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
+export default function Header() {
+    const word = dynamicContent[getRandomNumber(2)];
+  
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {word} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
