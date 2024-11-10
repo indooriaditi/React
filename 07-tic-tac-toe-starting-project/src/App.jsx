237d61a85be5ea11dcc76dad8fd3fb50b53d09e5
Aditi Indoori <indooriaditi@Aditis-MacBook-Pro-2.local>
2024-11-10 13:25:51 -0800

    disabling button

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,33 +1,36 @@
-import GameBoard from "./components/GameBoard"
-import Player from "./components/Player"
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
 import Log from "./components/Log";
 
 import { useState } from "react";
 
+function getActivePlayer(playerArray) {
+  let currentPlayer = "X";
+
+  // Switch active player
+  if (playerArray.length > 0 && playerArray[0].playerName === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [playerArray, setPlayerArray] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = getActivePlayer(playerArray);
 
   function handleActivePlayer(rowIndex, colIndex) {
-    setActivePlayer((prevActivePlayer) => prevActivePlayer === 'X'? 'O' : 'X');
-    
     setPlayerArray((prevPlayerArray) => {
-      let currentPlayer = 'X';
-
-      // Switch active player
-      if(prevPlayerArray.length > 0 && prevPlayerArray[0].playerName === 'X') {
-        currentPlayer = 'O';
-      }
+      const currentPlayer = getActivePlayer(prevPlayerArray);
 
       // After every turn, update the playerArray
       const updatedPlayerArray = [
-        { cell: { row: rowIndex, col: colIndex }, 
-          playerName: currentPlayer },
-       ...prevPlayerArray
+        { cell: { row: rowIndex, col: colIndex }, playerName: currentPlayer },
+        ...prevPlayerArray,
       ];
 
       return updatedPlayerArray;
-
     });
   }
 
@@ -35,14 +38,22 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player-1" symbol="X" isActive={activePlayer==='X'}/>
-          <Player initialName="Player-2" symbol="O" isActive={activePlayer==='O'}/>
+          <Player
+            initialName="Player-1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player-2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSquareSelect={handleActivePlayer} players={playerArray}/>
+        <GameBoard onSquareSelect={handleActivePlayer} players={playerArray} />
       </div>
-      <Log players={playerArray}/>
+      <Log players={playerArray} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,30 +1,35 @@
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
-export default function GameBoard({onSquareSelect, players}) {
-    let gameBoard = initialGameBoard;
+export default function GameBoard({ onSquareSelect, players }) {
+  let gameBoard = initialGameBoard;
 
-    for (const player of players) {
-        const {cell, playerName} = player;
-        gameBoard[cell.row][cell.col] = playerName;
-    }
+  for (const player of players) {
+    const { cell, playerName } = player;
+    gameBoard[cell.row][cell.col] = playerName;
+  }
 
-    return (
-        <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
-                <li key={rowIndex}>
-                    <ol>
-                        {row.map((cell, colIndex) => (
-                            <li key={colIndex}>
-                                <button onClick={() => onSquareSelect(rowIndex, colIndex)}>{cell}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((cell, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSquareSelect(rowIndex, colIndex)}
+                  disabled={cell !== null}
+                >
+                  {cell}
+                </button>
+              </li>
             ))}
-        </ol>
-    );
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
